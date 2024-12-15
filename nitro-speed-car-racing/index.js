bcb6561b890a6f126091684decdc652159be3079
buitrongtuan <154553857+pupuchibu@users.noreply.github.com>
2024-12-16 01:58:42 +0700

    178

---
+++ nitro-speed-car-racing/index.js
@@ -0,0 +1,10 @@
+window.screenOrientation = "sensor_landscape";
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.12.0/laya.core.js")
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.12.0/laya.ui.js")
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.12.0/laya.physics.js")
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.12.0/min/laya.vvmini.min.js")
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.12.0/min/laya.d3.min.js")
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.12.0/min/laya.physics3D.min.js")
+loadLib("js/main.js");
+
+
